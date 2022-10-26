ee1da05681ba74a3979116d9257a3c649d53a90c
zacherypan <zacherypanma@gmail.com>
2022-10-26 17:46:15 -0400

    MainTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 public class MainTeleOp {
+
+    // Gamepad 1
+    if(gamepad1.a) {
+        bot.claw.openClaw();
+        if(gamepad1.a) {
+            bot.claw.closeClaw();
+            return;
+        }
+    }
+
+    // Gamepad 2
+    double rightY = gamepad2.right_stick_y;
+    double leftY = gamepad2.left_stick_y;
+
+    if(rightY > 0.0) {
+        bot.slide.extendSlide();
+    } else {
+        bot.slide.retractSlide();
+    }
+
+    if(leftY > 0.0) {
+        bot.turret.rotateLeft();
+    } else {
+        bot.turret.rotateRight();
+    }
+
 }
