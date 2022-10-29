4da1982c1c632b01315228724697b08a63eb700f
mli2025 <mli2025@k12.andoverma.us>
2022-10-28 22:22:37 -0400

    more fixes + opencv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/easyopencv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/easyopencv.java
@@ -4,9 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvInternalCamera;
 
 public class easyopencv extends LinearOpMode {
-    OpenCvCamera camera;
-    AprilTagDetection aprilTagDetection;
+    OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    public static final double FEET_TO_METERS = 3.28084;
+
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/subsystems/Intake.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems;
 public class Bot {
 
     public static Bot instance;
+    public org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems.slide slide;
 
     public static Bot getInstance(){
         if(instance != null) {
