5089ea4a5976f57d6dfa8441e9554f1c6c006e7d
mli2025 <mli2025@k12.andoverma.us>
2022-11-02 17:36:06 -0400

    UGH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
@@ -1,55 +1,55 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
-
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems.Bot;
-import org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems.Claw;
-
-@TeleOp(name = "MainTeleOp", group = "Competition")
-public class MainTeleOp {
-    @Override
-    Gamepad gamepad1 = new Gamepad(gamepad1);
-    Gamepad gamepad2 = new Gamepad(gamepad2);
-    Claw claw = new Claw();
-
-    Bot bot = new Bot();
-
-    if(gamepad1.wasJustPressed(gamepad1.a)) {
-        Claw.openClaw();
-        if(gamepad1.a) {
-            Claw.closeClaw();
-            return;
-        }
-    }
-
-    if(gamepad1.wasJustPressed(gamepad1.x))
-    {
-        bot.realIntake.runIntake();
-        return;
-    }
-}
-
-        // Gamepad 2
-        double rightY = gamepad2.right_stick_y;
-        double leftY = gamepad2.left_stick_y;
-
-    if(rightY > 0.0) {
-            bot.slide.extendSlide();
-        }
-    else if(rightY < 0.0)
-    {
-        bot.slide.retractSlide();
-    }
-
-//    if(leftY > 0.0) {
-//            bot.turret.rotateLeft();
-//        } else {
-//            bot.turret.rotateRight();
-//        }
+//package org.firstinspires.ftc.teamcode.opmodes.teleop;
 //
+//import com.arcrobotics.ftclib.gamepad.GamepadEx;
+//import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+//import com.qualcomm.robotcore.hardware.Gamepad;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems.Bot;
+//import org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems.Claw;
+//
+//@TeleOp(name = "MainTeleOp", group = "Competition")
+//public class MainTeleOp extends LinearOpMode {
+//    @Override
+//    Gamepad gamepad1 = new Gamepad(gamepad1);
+//    Gamepad gamepad2 = new Gamepad(gamepad2);
+//    Claw claw = new Claw();
+//
+//    Bot bot = new Bot();
+//
+//    if(gamepad1.wasJustPressed(gamepad1.a)) {
+//        Claw.openClaw();
+//        if(gamepad1.a) {
+//            Claw.closeClaw();
+//            return;
+//        }
+//    }
+//
+//    if(gamepad1.wasJustPressed(gamepad1.x))
+//    {
+//        bot.realIntake.runIntake();
+//        return;
 //    }
 //}
+//
+//        // Gamepad 2
+//        double rightY = gamepad2.right_stick_y;
+//        double leftY = gamepad2.left_stick_y;
+//
+//    if(rightY > 0.0) {
+//            bot.slide.extendSlide();
+//        }
+//    else if(rightY < 0.0)
+//    {
+//        bot.slide.retractSlide();
+//    }
+//
+////    if(leftY > 0.0) {
+////            bot.turret.rotateLeft();
+////        } else {
+////            bot.turret.rotateRight();
+////        }
+////
+////    }
+////}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/subsystems/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/subsystems/Slide.java
@@ -1,28 +1,28 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class slide {
-
-    Gamepad gamepad2 = new Gamepad(gamepad2);
-    public HardwareMap hardwareMap;
-    DcMotor slideMotor = hardwareMap.dcMotor.get("slideMotor");
-
-    public void extendSlide() {
-        double y = gamepad2.right_stick_y;
-        if(y > 0.0) {
-            slideMotor.setPower(y);
-        }
-    }
-
-    public void retractSlide() {
-        double y = gamepad2.right_stick_y;
-        if(y < 0.0) {
-            slideMotor.setPower(y);
-        }
-    }
-
-}
+//package org.firstinspires.ftc.teamcode.opmodes.teleop.subsystems;
+//
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.arcrobotics.ftclib.gamepad.GamepadEx;
+//import com.qualcomm.robotcore.hardware.Gamepad;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//public class slide {
+//
+//    Gamepad gamepad2 = new Gamepad(gamepad2);
+//    public HardwareMap hardwareMap;
+//    DcMotor slideMotor = hardwareMap.dcMotor.get("slideMotor");
+//
+//    public void extendSlide() {
+//        double y = gamepad2.right_stick_y;
+//        if(y > 0.0) {
+//            slideMotor.setPower(y);
+//        }
+//    }
+//
+//    public void retractSlide() {
+//        double y = gamepad2.right_stick_y;
+//        if(y < 0.0) {
+//            slideMotor.setPower(y);
+//        }
+//    }
+//
+//}
