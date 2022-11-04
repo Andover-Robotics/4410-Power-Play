5f4145037f4481369798bdf71ed2ca6eddbf5c45
anikanagle <nikunagle@gmail.com>
2022-11-04 16:33:39 -0400

    Adding correct constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RoadRunnerTrial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RoadRunnerTrial.java
@@ -19,32 +19,5 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
         }
 
-     /*   @Override
-        public void runOpMode() throws InterruptedException {
-            RRMecanumDrive drive = new RRMecanumDrive(hardwareMap);
-
-            waitForStart();
-
-            if (isStopRequested()) {
-                return;
-            }
-
-            Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                    .splineTo(new Vector2d(30, 30), 0)
-                    .build();
-
-            drive.followTrajectory(traj);
-
-            sleep(2000);
-
-            drive.followTrajectory(
-                    drive.trajectoryBuilder(traj.end(), true)
-                            .splineTo(new Vector2d(0, 0), Math.toRadians(180))
-                            .build()
-            );
-        }
-    }
-
-      */
 
 }
