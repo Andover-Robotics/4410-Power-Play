3b8230c1ddd3e7a467fe4eddaeccbbb3c26a5e3d
anikanagle <nikunagle@gmail.com>
2022-11-04 16:25:50 -0400

    Fixing input errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RoadRunnerTrials/trajectorysequence/TrajectorySequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RoadRunnerTrials/trajectorysequence/TrajectorySequence.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.auto.RoadRunnerTrials.trajectorys
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.SequenceSegment;
+import org.firstinspires.ftc.teamcode.opmodes.auto.RoadRunnerTrials.trajectorysequence.sequencesegment.SequenceSegment;
 
 import java.util.Collections;
 import java.util.List;
