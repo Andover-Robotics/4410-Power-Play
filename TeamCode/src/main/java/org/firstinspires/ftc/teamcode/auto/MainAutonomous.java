938a48ffa4c4277e903fc71c9edac42416f1e246
atch2203 <atch2203@mit.edu>
2023-01-05 14:33:29 -0500

    put paths together

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -14,9 +16,13 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 public class MainAutonomous extends LinearOpMode {
 
     Bot bot;
+    boolean terminal = false;
     @Override
     public void runOpMode() throws InterruptedException {
         bot = Bot.getInstance(this);
+        bot.claw.open();
+
+        GamepadEx gp1 = new GamepadEx(gamepad1);
 
         int cameraID = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewID", "id", hardwareMap.appContext.getPackageName());
         OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.FRONT, cameraID);
@@ -35,21 +41,28 @@ public class MainAutonomous extends LinearOpMode {
 
             }
         });
+        while(!isStarted()) {
+            telemetry.addData("Current FPS:", camera.getFps());
+            telemetry.addData("Current Max FPS:", camera.getCurrentPipelineMaxFps());
 
-        telemetry.addData("Current FPS:", camera.getFps());
-        telemetry.addData("Current Max FPS:", camera.getCurrentPipelineMaxFps());
+            if (pipeline.getSignalVal() == TestPipeline.SignalVal.GREEN) {
+                telemetry.addData("The signal is green with a percentage of", TestPipeline.greenPercentage);
+            }
+            else if(pipeline.getSignalVal() == TestPipeline.SignalVal.PINK){
+                telemetry.addData("The signal is pink with a percentage of", TestPipeline.pinkPercentage);
+            }
+            else if(pipeline.getSignalVal() == TestPipeline.SignalVal.YELLOW){
+                telemetry.addData("The signal is yellow with a percentage of", TestPipeline.yellowPercentage);
+            }
 
-        waitForStart();
-        if (pipeline.getSignalVal() == TestPipeline.SignalVal.GREEN) {
-            telemetry.addData("The signal is green with a percentage of", TestPipeline.greenPercentage);
-        }
-        else if(pipeline.getSignalVal() == TestPipeline.SignalVal.PINK){
-            telemetry.addData("The signal is pink with a percentage of", TestPipeline.pinkPercentage);
+            telemetry.addData("\nDo terminal (press X to switch)", terminal);
+            gp1.readButtons();
+            if(gp1.wasJustPressed(GamepadKeys.Button.X)){
+                terminal = !terminal;
+            }
+            telemetry.update();
         }
 
-        else if(pipeline.getSignalVal() == TestPipeline.SignalVal.YELLOW){
-            telemetry.addData("The signal is yellow with a percentage of", TestPipeline.yellowPercentage);
-        }
         camera.stopStreaming();
 
         Pose2d startPose = new Pose2d();
@@ -66,13 +79,48 @@ public class MainAutonomous extends LinearOpMode {
                 parkLeft = bot.rr.trajectoryBuilder(startPose)
                         .back(24).build();
 
-        switch(pipeline.getSignalVal()){//TODO figure out which one is left/center/right
-            case GREEN:
-                break;
-            case PINK:
-                break;
-            case YELLOW:
-                break;
+        Trajectory moveForward = bot.rr.trajectoryBuilder(startPose)
+                .forward(28).build(),
+                parkStrafeRight = bot.rr.trajectoryBuilder(startPose)
+                .strafeRight(24).build(),
+                parkStrafeLeft = bot.rr.trajectoryBuilder(startPose)
+                        .strafeLeft(24).build();
+
+
+        if(terminal) {
+            bot.rr.followTrajectory(moveOut);
+
+            bot.rr.followTrajectory(goToTerminal);
+            bot.claw.open();
+            sleep(1000);
+            bot.rr.followTrajectory(goBack);
+
+            bot.rr.followTrajectory(strafeLeft);
+            switch(pipeline.getSignalVal()){//TODO figure out which one is left/center/right
+                case GREEN://LEFT
+                    bot.rr.followTrajectory(parkLeft);
+                    break;
+                case PINK://MIDDLE
+                    break;
+                case YELLOW://RIGHT
+                    bot.rr.followTrajectory(parkRight);
+                    break;
+            }
+        }else{
+            bot.rr.followTrajectory(moveForward);
+            switch(pipeline.getSignalVal()){//TODO figure out which one is left/center/right
+                case GREEN://LEFT
+                    bot.rr.followTrajectory(parkStrafeLeft);
+                    break;
+                case PINK://MIDDLE
+                    break;
+                case YELLOW://RIGHT
+                    bot.rr.followTrajectory(parkStrafeRight);
+                    break;
+            }
         }
+
+
+
     }
 }
