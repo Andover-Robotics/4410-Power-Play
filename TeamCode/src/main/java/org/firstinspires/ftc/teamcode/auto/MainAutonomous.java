7cbcfdd143d14f9c8df849b3f3507159719507d4
atch2203 <atch2203@gmail.com>
2023-01-11 13:28:08 -0500

    Fix auto slides/claw and fix memory leak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,29 +18,27 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.Map;
 
-@Autonomous(name="MainAutonomousPark")
+@Autonomous(name="MainAutonomous")
 public class MainAutonomous extends LinearOpMode {
 
     Bot bot;
-    boolean terminal = false;
     @Override
     public void runOpMode() throws InterruptedException {
-        for (Map.Entry<String, DcMotor> entry : hardwareMap.dcMotor.entrySet()) {
-            entry.getValue().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            while (!isStopRequested() && Math.abs(entry.getValue().getCurrentPosition()) > 1) {
-                idle();
-            }
-            telemetry.addData(entry.getKey(), "is reset");
-            telemetry.update();
-        }
+        telemetry.setAutoClear(true);
 
-        Bot.instance = null;
         bot = Bot.getInstance(this);
-        Thread closeClaw = new Thread(bot.claw::close);
-        closeClaw.start();
+
+        // Retrieve the IMU from the hardware map
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        // Technically this is the default, however specifying it is clearer
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        // Without this, data retrieving from the IMU throws an exception
+        imu.initialize(parameters);
 
         GamepadEx gp1 = new GamepadEx(gamepad1);
 
+        //CAMERA STUFF =====================
         TestPipeline pipeline = new TestPipeline(telemetry);
 
         WebcamName camName = hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -74,14 +73,22 @@ public class MainAutonomous extends LinearOpMode {
                 telemetry.addData("The signal is yellow with a percentage of", TestPipeline.yellowPercentage);
             }
 
-            /*telemetry.addData("\nDo terminal (press X to switch)", terminal);
             gp1.readButtons();
-            if(gp1.wasJustPressed(GamepadKeys.Button.X)){
-                terminal = !terminal;
+            if(gp1.wasJustPressed(GamepadKeys.Button.Y)) {
+                bot.claw.close();
             }
-         .   telemetry.update();
-             */
+
+            telemetry.update();
+
         }
+
+        camera.stopStreaming();
+        camera.closeCameraDevice();
+
+        //END CAMERA STUFF ===============
+
+        waitForStart();
+
         Thread slidePeriodic = new Thread(() -> {
             while(opModeIsActive()){
                 bot.slide.periodic();
@@ -89,72 +96,16 @@ public class MainAutonomous extends LinearOpMode {
         });
         slidePeriodic.start();
 
-        camera.stopStreaming();
-        camera.closeCameraDevice();
-
         Pose2d startPose = new Pose2d();
-        Trajectory moveOut = bot.rr.trajectoryBuilder(startPose)
-                .strafeLeft(40).build(),
-                score = bot.rr.trajectoryBuilder(new Pose2d(0, 40))//change this for more complex paths
-                        .forward(4).build(),
-                goBack = bot.rr.trajectoryBuilder(new Pose2d(4, 40))
-                        .back(4).build(),
-                strafeRight = bot.rr.trajectoryBuilder(new Pose2d(0, 40))
-                        .strafeRight(12).build(),
-                parkRight = bot.rr.trajectoryBuilder(new Pose2d(0, 28))
-                        .forward(24).build(),
-                parkLeft = bot.rr.trajectoryBuilder(new Pose2d(0, 28))
-                        .back(24).build();
+        Trajectory forward = bot.rr.trajectoryBuilder(startPose).forward(4).build(),
+                back = bot.rr.trajectoryBuilder(new Pose2d(4, 0)).back(4).build();
 
-        Trajectory strafeLeftfirst = bot.rr.trajectoryBuilder(startPose)
-                .strafeLeft(28).build();
+        bot.slide.runToLow();
+        bot.rr.followTrajectory(forward);
 
-
-
-            bot.rr.followTrajectory(moveOut);
-            Thread runToTop = new Thread(bot.slide::runToTop);
-            bot.slide.runToTop();
-            telemetry.addLine("Started slide.");
-            telemetry.update();
-            sleep(3000);
-            bot.rr.followTrajectory(score);
-            //bot.claw.open();
-            Thread openClaw = new Thread(bot.claw::open);
-            telemetry.addData("Started claw", openClaw);
-            telemetry.update();
-            openClaw.start();
-            sleep(1000);
-            bot.rr.followTrajectory(goBack);
-            Thread runToBottom = new Thread(bot.slide::runToBottom);
-            bot.slide.runToBottom();
-            telemetry.addData("Started slide",runToBottom);
-            telemetry.update();
-            sleep(3000);
-            bot.rr.followTrajectory(strafeRight);
-            switch(pipeline.getSignalVal()){//TODO figure out which one is left/center/right
-                case GREEN://LEFT
-                    bot.rr.followTrajectory(parkLeft);
-                    break;
-                case PINK://MIDDLE
-                    break;
-                case YELLOW://RIGHT
-                    bot.rr.followTrajectory(parkRight);
-                    break;
-            }
-
-            /*bot.rr.followTrajectory(strafeLeftfirst);
-            switch(pipeline.getSignalVal()){
-                case GREEN://LEFT
-                    bot.rr.followTrajectory(parkLeft);
-                    break;
-                case PINK://MIDDLE
-                    break;
-                case YELLOW://RIGHT
-                    bot.rr.followTrajectory(parkRight);
-                    break;
-            */
-        slidePeriodic.interrupt();
-        }
-
+        bot.claw.open();
+        bot.rr.followTrajectory(back);
     }
+
+}
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/TestPipeline.java
@@ -35,38 +35,38 @@ public class TestPipeline extends OpenCvPipeline{
     Scalar pinkLowHSV= new Scalar(134,73,80);
     Scalar pinkHighHSV = new Scalar(180,225,246);
 
+    Mat greenMat = new Mat();
+    Mat yellowMat = new Mat();
+    Mat pinkMat = new Mat();
+    Mat smallMat = new Mat();
+
     @Override
     public Mat processFrame(Mat input) {
         //176 x 144= matrix size
-        //
-
         Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2HSV);
         ROI.x = input.width()/2 + horizOffset - 25;
         ROI.y = input.height()/2 + vertOffset - 25;
         ROI.width = width;
         ROI.height = height;
 
+        smallMat = input.submat(ROI);
 
-        //mask: takes image and tells which pixels to copy and which to leave black
+        Core.inRange(smallMat,greenLowHSV,greenHighHSV, greenMat);
 
+        greenPercentage = (Core.sumElems(greenMat)).val[0];
 
+        Core.inRange(smallMat, yellowLowHSV, yellowHighHSV, yellowMat);
 
-        Mat epicSubmat = input.submat(ROI);
+        yellowPercentage = (Core.sumElems(yellowMat)).val[0];
 
+        Core.inRange(smallMat,pinkLowHSV,pinkHighHSV,pinkMat);
 
-        Core.inRange(input.submat(ROI),greenLowHSV,greenHighHSV, epicSubmat);
+        pinkPercentage = (Core.sumElems(pinkMat)).val[0];
 
-
-        greenPercentage = (Core.sumElems(epicSubmat)).val[0];
-
-
-        Core.inRange(input.submat(ROI), yellowLowHSV, yellowHighHSV, epicSubmat);
-
-        yellowPercentage = (Core.sumElems(epicSubmat)).val[0];
-
-        Core.inRange(input.submat(ROI),pinkLowHSV,pinkHighHSV,epicSubmat);
-
-        pinkPercentage = (Core.sumElems(epicSubmat)).val[0];
+        pinkMat.release();
+        yellowMat.release();
+        greenMat.release();
+        smallMat.release();
 
         //Math.round((Core.sumElems(epicSubmat).val[0]/ROI.area()/255)*100);
 
@@ -77,26 +77,27 @@ public class TestPipeline extends OpenCvPipeline{
 
         if(greatestNum == greenPercentage){
             signalVal = SignalVal.GREEN;
-            telemetry.addData("The signal value is green with a percentage of", greenPercentage);
-            telemetry.update();
+//            telemetry.addData("The signal value is green with a percentage of", greenPercentage);
+//            telemetry.update();
         }else if(greatestNum == yellowPercentage){
             signalVal = SignalVal.YELLOW;
-            telemetry.addData("The signal value is yellow with a percentage of",yellowPercentage);
-            telemetry.update();
+//            telemetry.addData("The signal value is yellow with a percentage of",yellowPercentage);
+//            telemetry.update();
         }else if(greatestNum == pinkPercentage){
             signalVal = SignalVal.PINK;
-            telemetry.addData("The signal value is red with a percentage of",pinkPercentage);
-            telemetry.update();
+//            telemetry.addData("The signal value is red with a percentage of",pinkPercentage);
+//            telemetry.update();
         }
-        telemetry.addData("Green is",greenPercentage);
-        telemetry.addData("Yellow is",yellowPercentage);
-        telemetry.addData("Pink is",pinkPercentage);
-        telemetry.addData("Matrix size",input.size());
-        telemetry.addData("I think it's", getSignalVal().toString());
-        telemetry.update();
+//        telemetry.addData("Green is",greenPercentage);
+//        telemetry.addData("Yellow is",yellowPercentage);
+//        telemetry.addData("Pink is",pinkPercentage);
+//        telemetry.addData("Matrix size",input.size());
+//        telemetry.addData("I think it's", getSignalVal().toString());
+//        telemetry.update();
 
-        Imgproc.rectangle(input, ROI, new Scalar(255, 255, 255));
-        Imgproc.cvtColor(input, input, Imgproc.COLOR_HSV2RGB);
+        input.release();
+//        Imgproc.rectangle(input, ROI, new Scalar(255, 255, 255));
+//        Imgproc.cvtColor(input, input, Imgproc.COLOR_HSV2RGB);
 
         return input;
 
