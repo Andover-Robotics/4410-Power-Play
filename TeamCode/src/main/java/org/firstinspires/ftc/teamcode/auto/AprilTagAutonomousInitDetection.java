c38dafef01e2b3c522a408930611a93369bc19e3
atch2203 <atch2203@gmail.com>
2023-01-25 11:16:31 -0500

    add apriltags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AprilTagAutonomousInitDetection.java
@@ -0,0 +1,176 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@TeleOp
+public class AprilTagAutonomousInitDetection extends LinearOpMode {
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+
+    //720p
+    double fx = 1078.03779;
+    double fy = 1084.50988;
+    double cx = 580.850545;
+    double cy = 245.959325;
+
+    // UNITS ARE METERS
+    double tagsize = 0.032; //ONLY FOR TESTING
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int ID_ONE = 1;
+    int ID_TWO = 2;
+    int ID_THREE = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
+    @Override
+    public void runOpMode()
+    {
+        //This is for cam config, if we are going to use the phones cam then we will most likely need to edit this
+        //However, if we use an external webcam, then we won't have to edit anything here.
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //Note: When config a cam, the name will already be "Webcam 1".
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == ID_ONE || tag.id == ID_TWO || tag.id == ID_THREE)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+        if (tagOfInterest == null || tagOfInterest.id == ID_ONE) {
+            //Park
+        } else if (tagOfInterest.id == ID_TWO){
+            //Park
+        } else{
+            //Park
+        }
+
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+        while (opModeIsActive()) {sleep(20);}
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MainAutonomous.java
@@ -1,163 +1,351 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.auto.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.auto.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.teleop.subsystems.Bot;
+import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
+import java.util.ArrayList;
 import java.util.Map;
 
-@Autonomous(name="MainAutonomousPark")
+@Autonomous(name="MainAutonomous")
 public class MainAutonomous extends LinearOpMode {
 
     Bot bot;
-    boolean terminal = false;
+    boolean isRight;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    double fx = 1078.03779;
+    double fy = 1084.50988;
+    double cx = 580.850545;
+    double cy = 245.959325;
+
+    // UNITS ARE METERS
+    double tagsize = 0.032; //ONLY FOR TESTING
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int ID_ONE = 1;
+    int ID_TWO = 2;
+    int ID_THREE = 3;
+    AprilTagDetection tagOfInterest = null;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        for (Map.Entry<String, DcMotor> entry : hardwareMap.dcMotor.entrySet()) {
-            entry.getValue().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            while (!isStopRequested() && Math.abs(entry.getValue().getCurrentPosition()) > 1) {
-                idle();
-            }
-            telemetry.addData(entry.getKey(), "is reset");
-            telemetry.update();
-        }
-
-        Bot.instance = null;
+        telemetry.setAutoClear(true);
         bot = Bot.getInstance(this);
-        Thread closeClaw = new Thread(bot.claw::close);
-        closeClaw.start();
+
+        // Retrieve the IMU from the hardware map
+//        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        // Technically this is the default, however specifying it is clearer
+//        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+//        // Without this, data retrieving from the IMU throws an exception
+//        imu.initialize(parameters);
 
         GamepadEx gp1 = new GamepadEx(gamepad1);
 
-        TestPipeline pipeline = new TestPipeline(telemetry);
+        //CAMERA STUFF =====================
 
         WebcamName camName = hardwareMap.get(WebcamName.class, "Webcam 1");
         OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(camName);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+        AprilTagDetectionPipeline aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
             @Override
-            public void onOpened() {
-                camera.setPipeline(pipeline);
-                camera.startStreaming(320 * 3, 240 * 3, OpenCvCameraRotation.UPRIGHT);
-                telemetry.addData("Camera Status", "Opened");
+            public void onOpened()
+            {
+                camera.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode) {
-                telemetry.addData("Error Code", errorCode);
+            public void onError(int errorCode)
+            {
+
             }
         });
+
+        telemetry.setMsTransmissionInterval(50);
 
 
 
         while(!isStarted()) {
+            if(gp1.wasJustPressed(GamepadKeys.Button.X)){
+                isRight=!isRight;
+            }
             telemetry.addData("Current FPS:", camera.getFps());
             telemetry.addData("Current Max FPS:", camera.getCurrentPipelineMaxFps());
+            telemetry.addData("Is right side?", isRight);
 
-            if (pipeline.getSignalVal() == TestPipeline.SignalVal.GREEN) {
-                telemetry.addData("The signal is green with a percentage of", TestPipeline.greenPercentage);
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == ID_ONE || tag.id == ID_TWO || tag.id == ID_THREE)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
             }
-            else if(pipeline.getSignalVal() == TestPipeline.SignalVal.PINK){
-                telemetry.addData("The signal is pink with a percentage of", TestPipeline.pinkPercentage);
-            }
-            else if(pipeline.getSignalVal() == TestPipeline.SignalVal.YELLOW){
-                telemetry.addData("The signal is yellow with a percentage of", TestPipeline.yellowPercentage);
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
             }
 
-            /*telemetry.addData("\nDo terminal (press X to switch)", terminal);
+            telemetry.update();
+            sleep(20);
+
             gp1.readButtons();
-            if(gp1.wasJustPressed(GamepadKeys.Button.X)){
-                terminal = !terminal;
+            if(gp1.wasJustPressed(GamepadKeys.Button.Y)) {
+                bot.claw.close();
             }
-         .   telemetry.update();
-             */
+
+            telemetry.update();
+
         }
+
+        camera.stopStreaming();
+        camera.closeCameraDevice();
+
+        //END CAMERA STUFF ===============
+
+        waitForStart();
+
+        bot.claw.close();
+
         Thread slidePeriodic = new Thread(() -> {
             while(opModeIsActive()){
                 bot.slide.periodic();
             }
         });
         slidePeriodic.start();
-
-        camera.stopStreaming();
-        camera.closeCameraDevice();
-
-        Pose2d startPose = new Pose2d();
-        Trajectory moveOut = bot.rr.trajectoryBuilder(startPose)
-                .strafeLeft(40).build(),
-                score = bot.rr.trajectoryBuilder(new Pose2d(0, 40))//change this for more complex paths
-                        .forward(4).build(),
-                goBack = bot.rr.trajectoryBuilder(new Pose2d(4, 40))
-                        .back(4).build(),
-                strafeRight = bot.rr.trajectoryBuilder(new Pose2d(0, 40))
-                        .strafeRight(12).build(),
-                parkRight = bot.rr.trajectoryBuilder(new Pose2d(0, 28))
-                        .forward(24).build(),
-                parkLeft = bot.rr.trajectoryBuilder(new Pose2d(0, 28))
-                        .back(24).build();
-
-        Trajectory strafeLeftfirst = bot.rr.trajectoryBuilder(startPose)
-                .strafeLeft(28).build();
+        bot.claw.close();
 
 
 
-            bot.rr.followTrajectory(moveOut);
-            Thread runToTop = new Thread(bot.slide::runToTop);
-            runToTop.start();
-            telemetry.addLine("Started slide.");
-            telemetry.update();
-            sleep(3000);
-            bot.rr.followTrajectory(score);
-            //bot.claw.open();
-            Thread openClaw = new Thread(bot.claw::open);
-            telemetry.addData("Started claw", openClaw);
-            telemetry.update();
-            openClaw.start();
-            sleep(1000);
-            bot.rr.followTrajectory(goBack);
-            Thread runToBottom = new Thread(bot.slide::runToBottom);
-            runToBottom.start();
-            telemetry.addData("Started slide",runToBottom);
-            telemetry.update();
-            sleep(3000);
-            bot.rr.followTrajectory(strafeRight);
-            switch(pipeline.getSignalVal()){//TODO figure out which one is left/center/right
-                case GREEN://LEFT
-                    bot.rr.followTrajectory(parkLeft);
-                    break;
-                case PINK://MIDDLE
-                    break;
-                case YELLOW://RIGHT
-                    bot.rr.followTrajectory(parkRight);
-                    break;
+        Pose2d startPose = new Pose2d(0,0,0);
+        Pose2d park1Pose = new Pose2d(12, 0,0);
+        Pose2d park2Pose = new Pose2d(12, 12, 0);
+
+
+        Trajectory forward = bot.rr.trajectoryBuilder(startPose)
+                .forward(48)
+                .build();
+
+
+        //Alliance 1 trajectories
+        Trajectory alliance1StrafeRight = bot.rr.trajectoryBuilder(forward.end())
+                .strafeRight(13)
+                .build();
+        Trajectory alliance1ApproachJunction = bot.rr.trajectoryBuilder(alliance1StrafeRight.end())
+                .forward(6)
+                .build();
+        Trajectory alliance1GoBack = bot.rr.trajectoryBuilder(alliance1ApproachJunction.end())
+                .back(4)
+                .build();
+        Trajectory alliance1GoToCone = bot.rr.trajectoryBuilder(alliance1GoBack.end())
+                .strafeLeft(28)
+                .build();
+        Trajectory alliance1GoForwardToCone = bot.rr.trajectoryBuilder(new Pose2d(alliance1GoToCone.end().getX(), alliance1GoToCone.end().getY(), Math.toRadians(90)))
+                .forward(4)
+                .build();
+        Trajectory alliance1GoToJunction = bot.rr.trajectoryBuilder(new Pose2d(alliance1GoForwardToCone.end().getX(), alliance1GoForwardToCone.end().getY(), Math.toRadians(0)))
+                .strafeRight(32)
+                .build();
+
+       /* Trajectory goBack = bot.rr.trajectoryBuilder(new Pose2d(allianceOneGoToJunction.end().getX(), allianceOneGoToJunction.end().getY(), -Math.toRadians(90)))
+                .back(6)
+                .build();
+        */
+
+        //Alliance 2 trajectories
+        Trajectory alliance2StrafeLeft= bot.rr.trajectoryBuilder(forward.end())
+                .strafeLeft(13)
+                .build();
+        Trajectory alliance2ApproachJunction= bot.rr.trajectoryBuilder(alliance2StrafeLeft.end())
+                .forward(6)
+                .build();
+        Trajectory alliance2GoBack = bot.rr.trajectoryBuilder(alliance2ApproachJunction.end())
+                .back(4)
+                .build();
+        Trajectory alliance2GoToCone = bot.rr.trajectoryBuilder(alliance2GoBack.end())
+                .strafeRight(28)
+                .build();
+        Trajectory alliance2GoForwardToCone = bot.rr.trajectoryBuilder(new Pose2d(alliance2GoToCone.end().getX(), alliance2GoToCone.end().getY(), -Math.toRadians(90)))
+                .forward(4)
+                .build();
+        Trajectory alliance2GoToJunction = bot.rr.trajectoryBuilder(new Pose2d(alliance2GoForwardToCone.end().getX(), alliance2GoForwardToCone.end().getY(), Math.toRadians(0)))
+                .strafeLeft(32)
+                .build();
+
+
+        bot.rr.followTrajectory(forward);
+
+        if(!isRight) {
+            bot.slide.runToTop();
+            bot.rr.followTrajectory(alliance1StrafeRight);
+
+            bot.rr.followTrajectory(alliance1ApproachJunction);
+            bot.slide.goDown();
+            sleep(100);
+            bot.claw.open();
+            //Cone placed
+            bot.rr.followTrajectory(alliance1GoBack);
+            bot.slide.runTo(580);
+            bot.rr.followTrajectory(alliance1GoToCone);
+            bot.rr.turn(Math.toRadians(90));
+            bot.rr.followTrajectory(alliance1GoForwardToCone);
+            bot.claw.close();
+            //new cone picked up
+            sleep(400);
+            bot.slide.runToTop();
+            bot.rr.turn(-Math.toRadians(90));
+            bot.rr.followTrajectory(alliance1GoToJunction);
+
+            bot.rr.followTrajectory(alliance1ApproachJunction);
+            bot.slide.goDown();
+            sleep(100);
+            bot.claw.open();
+            //cone placed
+            for(int i =0; i< 5; i++){
+                int runTo = 460;
+                bot.rr.followTrajectory(alliance1GoBack);
+                bot.slide.runTo(runTo);
+                bot.rr.followTrajectory(alliance1GoToCone);
+                bot.rr.turn(Math.toRadians(90));
+                bot.rr.followTrajectory(alliance1GoForwardToCone);
+                bot.claw.close();
+                //new cone picked up
+                sleep(400);
+                bot.slide.runToTop();
+                bot.rr.turn(-Math.toRadians(90));
+                bot.rr.followTrajectory(alliance1GoToJunction);
+
+                bot.rr.followTrajectory(alliance1ApproachJunction);
+                bot.slide.goDown();
+                sleep(100);
+                bot.claw.open();
+
+                runTo -= 70;
             }
-
-            /*bot.rr.followTrajectory(strafeLeftfirst);
-            switch(pipeline.getSignalVal()){
-                case GREEN://LEFT
-                    bot.rr.followTrajectory(parkLeft);
-                    break;
-                case PINK://MIDDLE
-                    break;
-                case YELLOW://RIGHT
-                    bot.rr.followTrajectory(parkRight);
-                    break;
-            */
-        //slidePeriodic.interrupt();
+            bot.rr.followTrajectory(alliance1GoBack);
         }
 
+        else {
+
+            bot.slide.runToTop();
+            bot.rr.followTrajectory(alliance2StrafeLeft);
+
+            bot.rr.followTrajectory(alliance2ApproachJunction);
+            bot.slide.goDown();
+            sleep(100);
+            bot.claw.open();
+            //Cone placed
+            bot.rr.followTrajectory(alliance2GoBack);
+            bot.slide.runTo(580);
+            bot.rr.followTrajectory(alliance2GoToCone);
+            bot.rr.turn(Math.toRadians(90));
+            bot.rr.followTrajectory(alliance2GoForwardToCone);
+            bot.claw.close();
+            //new cone picked up
+            sleep(400);
+            bot.slide.runToTop();
+            bot.rr.turn(-Math.toRadians(90));
+            bot.rr.followTrajectory(alliance2GoToJunction);
+
+            bot.rr.followTrajectory(alliance2ApproachJunction);
+            bot.slide.goDown();
+            sleep(100);
+            bot.claw.open();
+            //cone placed
+            bot.rr.followTrajectory(alliance2GoBack);
+            bot.slide.runTo(460);
+            bot.rr.followTrajectory(alliance2GoToCone);
+            bot.rr.turn(Math.toRadians(90));
+            bot.rr.followTrajectory(alliance2GoForwardToCone);
+            bot.claw.close();
+            //new cone picked up
+            sleep(400);
+            bot.slide.runToTop();
+            bot.rr.turn(-Math.toRadians(90));
+            bot.rr.followTrajectory(alliance2GoToJunction);
+
+            bot.rr.followTrajectory(alliance2ApproachJunction);
+            bot.slide.goDown();
+            sleep(100);
+            bot.claw.open();
+            bot.rr.followTrajectory(alliance2GoBack);
+
+        }
+        bot.slide.runToBottom();
+        slidePeriodic.interrupt();
     }
+
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+}
 
